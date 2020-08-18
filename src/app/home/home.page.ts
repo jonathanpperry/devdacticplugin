59f5e298dc57e07f6eb360085e30ec3a28bf6678
Jonathan Perry <jpper@umich.edu>
2020-08-19 00:09:13 +0900

    Commit of files to test first capacitor plugin

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+import "contacts-plugin";
+
+const { ContactsPlugin } = Plugins;
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+export class HomePage {
+  contacts = [];
+  constructor() {}
+
+  async loadContacts() {
+    this.contacts = (
+      await ContactsPlugin.getContacts("whatever filter")
+    ).results;
+    console.log("my contacts: ", this.contacts);
+  }
+}
